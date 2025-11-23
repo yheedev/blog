becae059573870c145d0525c28bcb817f5d04db1
yheedev <yheedev@gmail.com>
2025-11-23 19:52:02 +0900

    feat: 새로운 경로(resume, guest) 추가

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,8 +2,8 @@ const headerNavLinks = (lang: string = 'ko') => [
   { href: `/${lang}/about`, title: 'Blog' },
   { href: `/${lang}/tags`, title: 'Tags' },
   { href: `/${lang}/projects`, title: 'Projects' },
-  { href: `/${lang}/opensource`, title: 'OpenSource' },
-  { href: `/${lang}/about`, title: 'About' },
+  { href: `/${lang}/resume`, title: 'Resume' },
+  { href: `/${lang}/guest`, title: 'Guest' },
 ]
 
 export default headerNavLinks

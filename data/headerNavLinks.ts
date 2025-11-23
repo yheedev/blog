0436555024c4606b75b629f4f83650061008bee1
yheedev <yheedev@gmail.com>
2025-11-23 20:36:38 +0900

    feat: blog와 about 경로 알맞게 연결

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,6 @@
 const headerNavLinks = (lang: string = 'ko') => [
-  { href: `/${lang}/about`, title: 'Blog' },
+  { href: `/${lang}/blog`, title: 'Blog' },
+  { href: `/${lang}/about`, title: 'About' },
   { href: `/${lang}/tags`, title: 'Tags' },
   { href: `/${lang}/projects`, title: 'Projects' },
   { href: `/${lang}/resume`, title: 'Resume' },
